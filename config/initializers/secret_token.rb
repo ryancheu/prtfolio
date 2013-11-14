03d4757e44b4fb7cdcb05d629cf4d01622d10961
Patricia Saylor <psaylor42@gmail.com>
2013-11-14 00:48:47 -0500

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SaylorpAnuhyagIsabellatrombaRyancheuFinal::Application.config.secret_key_base = '5d72acc3c4ac53603d3f5954ebe8f20545a38b1d066c1deffc8b2ebc40ec6e5448b67a68c9ec54d25e2450ce3cb022418190b89c4620cff64455ff94d4efaecb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SaylorpAnuhyagIsabellatrombaRyancheuFinal::Application.config.session_store :cookie_store, key: '_saylorp_anuhyag_Isabellatromba_ryancheu_final_session'
