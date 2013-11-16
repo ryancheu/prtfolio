2d77229ed85524c4a299394a34300f88a82e526d
Ryan Cheu <ryan@twitchaudio.xvm.mit.edu>
2013-11-16 18:38:04 -0500

    S3/images working

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,9 @@
 class Image < ActiveRecord::Base
   has_one :block, as: :resource
+  
+  has_attached_file :content, 
+  :styles => {
+    thumb: '100x100>'
+  }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131116221118) do
+ActiveRecord::Schema.define(version: 20131116223550) do
 
   create_table "blocks", force: true do |t|
     t.integer  "page_index"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 20131116221118) do
   create_table "images", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "content_file_name"
+    t.string   "content_content_type"
+    t.integer  "content_file_size"
+    t.datetime "content_updated_at"
   end
 
   create_table "portfolios", force: true do |t|
