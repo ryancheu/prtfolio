3de896e3304677eab8d9188d86fd56ede3c6fab7
Patricia Saylor <psaylor42@gmail.com>
2013-11-17 12:37:05 -0500

    Removed redundant templates, and made the templates accept local variables to display and default to static variables

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,62 +1,7 @@
+#  primary author: saylorp
 class TemplatesController < ApplicationController
-  before_action :set_template, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @templates = Template.all
-  end
-
+  # Show the template with this id
   def show
+    @id = params[:id]
   end
-
-  def new
-    @template = Template.new
-  end
-
-  def edit
-  end
-
-  def create
-    @template = Template.new(template_params)
-
-    respond_to do |format|
-      if @template.save
-        format.html { redirect_to @template, notice: 'Template was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @template }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @template.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @template.update(template_params)
-        format.html { redirect_to @template, notice: 'Template was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @template.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @template.destroy
-    respond_to do |format|
-      format.html { redirect_to templates_url }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_template
-      @template = Template.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def template_params
-      params[:template]
-    end
 end
--- app/views/templates/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @template, :created_at, :updated_at
