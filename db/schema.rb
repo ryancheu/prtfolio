61667c5670eb23ac2c164e6aaef2f258827b46cd
Patricia Saylor <psaylor42@gmail.com>
2013-11-16 21:56:01 -0500

    Removed resources MVC

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131116223550) do
+ActiveRecord::Schema.define(version: 20131117025441) do
 
   create_table "blocks", force: true do |t|
     t.integer  "page_index"
@@ -55,12 +55,6 @@ ActiveRecord::Schema.define(version: 20131116223550) do
 
   create_table "projects", force: true do |t|
     t.integer  "portfolio_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "resources", force: true do |t|
-    t.integer  "block_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
