4f3c0b16644c484e07e3044b2db7bb971a0d8e02
ryancheu <ryancheu@mit.edu>
2013-11-17 17:01:10 -0500

    WIP portfolio

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,14 +1,15 @@
 class Project < ActiveRecord::Base
-	belongs_to :portfolio
-    has_one :user, through: :portfolio
-	has_many :blocks
-	has_one :theme
-
-    validates :portfolio, presence: true
+  belongs_to :portfolio
+  has_one :user, through: :portfolio
+  has_many :blocks
+  has_one :theme
+  belongs_to :image
+  
+  validates :portfolio, presence: true
     
-    # Returns the user who owns this block
-    def get_owner
-        return self.user
-    end
+  # Returns the user who owns this block
+  def get_owner
+    return self.user
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131117035818) do
+ActiveRecord::Schema.define(version: 20131117195817) do
 
   create_table "blocks", force: true do |t|
     t.integer  "page_index"
@@ -57,8 +57,8 @@ ActiveRecord::Schema.define(version: 20131117035818) do
     t.integer  "portfolio_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "title"
     t.text     "description"
+    t.string   "title"
     t.integer  "image_id"
   end
 
