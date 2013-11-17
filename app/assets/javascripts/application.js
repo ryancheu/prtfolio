82d110122a594e930ed2823998f2642c15f931a2
Isabella Tromba <isabella.tromba@gmail.com>
2013-11-17 17:33:30 -0500

    updated to fix scrolling issue

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,33 +17,5 @@
 //= require jquery.onepage-scroll
 //= require_tree .
 
-$(document).ready(function() {
-	$(".main").onepage_scroll({
-	   sectionContainer: "section", 
-	   easing: "ease",
-	   animationTime: 1000,
-	   pagination: true,
-	   updateURL: false,
-	   beforeMove: function(index) {
-	   	console.log(index);
-	   	fns = [
-	   		function() {
-	   			$('#welcome').addClass('active');
-	   			$('#features').removeClass('active');
-	   			$('#aboutus').removeClass('active');
-	   		},
-	   		function() {
-	   			$('#welcome').removeClass('active');
-	   			$('#features').addClass('active');
-	   			$('#aboutus').removeClass('active');
-	   		},
-	   		function() {
-	   			$('#welcome').removeClass('active');
-	   			$('#features').removeClass('active');
-	   			$('#aboutus').addClass('active');
-	   		}
-	   	];
-	   	fns[index-1]();
-	   }
-	});
-});
+$(document).ready(onReady);
+$(document).on('page:load', onReady);
