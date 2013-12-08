9b3533d25ada98126c81fec4d7642acfd02e75a6
ryancheu <ryancheu@mit.edu>
2013-12-07 21:22:48 -0500

    Remove alerts for creation/edit success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to Portfl.io!"
       @portfolio = current_user.create_portfolio()
       @portfolio.save
       redirect_to @user.portfolio
