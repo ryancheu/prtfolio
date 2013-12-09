a04306c5b883109bde07d2b3b9322917d04ec983
Patricia Saylor <psaylor42@gmail.com>
2013-12-09 03:58:56 -0500

    Removed autohtml initializer

--- config/initializers/autohtml.rb
+++ config/initializers/autohtml.rb
@@ -1,31 +1,31 @@
 # http://stackoverflow.com/questions/15558781/auto-html-breaking-rails-migrations
-if File.basename($0) == 'rake' && ARGV.grep(/db:/)
-  class ActiveRecord::Base
-    def self.auto_html_for(field, &block)
-        #nothing
-    end
-  end
+# if File.basename($0) == 'rake' && ARGV.grep(/db:/)
+#   class ActiveRecord::Base
+#     def self.auto_html_for(field, &block)
+#         #nothing
+#     end
+#   end
 
-else
+# else
 
-  AutoHtml.add_filter(:strip_tags).with({}) do |text, options|
-    view = ActionView::Base.new
-    view.strip_tags text
-  end
-  AutoHtml.add_filter(:autolink).with({}) do |text, options|
-    view = ActionView::Base.new
-    view.auto_link(text, :html => { :target => '_blank', :rel => "nofollow" })
-  end
+#   AutoHtml.add_filter(:strip_tags).with({}) do |text, options|
+#     view = ActionView::Base.new
+#     view.strip_tags text
+#   end
+#   AutoHtml.add_filter(:autolink).with({}) do |text, options|
+#     view = ActionView::Base.new
+#     view.auto_link(text, :html => { :target => '_blank', :rel => "nofollow" })
+#   end
 
 
-  class ActiveRecord::Base
-    class << self
-      alias_method :auto_html_for!, :auto_html_for
-    end
+#   class ActiveRecord::Base
+#     class << self
+#       alias_method :auto_html_for!, :auto_html_for
+#     end
 
-    def self.auto_html_for(field, &block)
-      auto_html_for!(field, &block)
-    end
-  end
+#     def self.auto_html_for(field, &block)
+#       auto_html_for!(field, &block)
+#     end
+#   end
 
-end
+# end
