537d81dbe7dc6cf18852e5b524ea935cc82e6a1b
Patricia Saylor <psaylor42@gmail.com>
2013-12-09 02:36:17 -0500

    Uncommented autohtml code

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,15 +2,15 @@
 class Video < ActiveRecord::Base
   has_one :block, as: :resource
 
-  # set up the auto_html logic that will embed images
-  # auto_html_for :video do
-  #   html_escape
-  #   image
-  #   youtube(:width => 350, :height => 350)
-  #   vimeo(:width => 350, :height => 350)
-  #   link :target => "_blank", :rel => "nofollow"
-  #   simple_format
-  # end
+  set up the auto_html logic that will embed images
+  auto_html_for :video do
+    html_escape
+    image
+    youtube(:width => 350, :height => 350)
+    vimeo(:width => 350, :height => 350)
+    link :target => "_blank", :rel => "nofollow"
+    simple_format
+  end
 
   def partial_path
     return "videos/video"
