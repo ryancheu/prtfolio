bf128100c203e5179ac9b17502320857d94c837e
Patricia Saylor <psaylor42@gmail.com>
2013-12-09 02:27:52 -0500

    Disabled auto_html for pushing to heroku

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,14 +3,14 @@ class Video < ActiveRecord::Base
   has_one :block, as: :resource
 
   # set up the auto_html logic that will embed images
-  auto_html_for :video do
-    html_escape
-    image
-    youtube(:width => 350, :height => 350)
-    vimeo(:width => 350, :height => 350)
-    link :target => "_blank", :rel => "nofollow"
-    simple_format
-  end
+  # auto_html_for :video do
+  #   html_escape
+  #   image
+  #   youtube(:width => 350, :height => 350)
+  #   vimeo(:width => 350, :height => 350)
+  #   link :target => "_blank", :rel => "nofollow"
+  #   simple_format
+  # end
 
   def partial_path
     return "videos/video"
